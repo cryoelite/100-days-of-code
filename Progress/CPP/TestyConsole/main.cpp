66a099a460bb210fae370d9af3a875fa700a7cb5
cryoelite <48724777+cryoelite@users.noreply.github.com>
2020-05-19 23:41:52 +0530

    Day35
    C++ , uniform initialization, factorial using recursion using void return type , pointers.

--- Progress/CPP/TestyConsole/main.cpp
+++ Progress/CPP/TestyConsole/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "math.h"
 
-void main() {
-	using namespace std;
-	cout << "Hello!" << " Covid's BAD!" ;
+using namespace std;
+
+int main() {
+	int c{ 12 }, d{ 33 }, e{ 1 };
+	int* a{ &c }, * b{ &d }, * result{ &e };
+	cout << Add(a, b);
+	AddVal(a, b,result);
+	cout << endl << c << " " << d << " " << e << endl;
+	e = 1;
+	Swap(a, b);
+	cout << endl << c << " " << d << endl;
+	d = 4;
+	Factorial(b, result);
+	cout << *result;
+
 }
