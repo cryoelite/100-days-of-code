0666b8f3aec81a5e557d1d164dc3aecc36351bdb
Arvind Sagar <48724777+cryoelite@users.noreply.github.com>
2020-06-01 13:16:44 +0530

    temp1

--- Progress/CPP/TestyConsole/main.cpp
+++ Progress/CPP/TestyConsole/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
-#include "Classy.h"
+#include "Dodo.h"
 
 
-using namespace std;
+
 
 int main() {
-	const char a[6] = "abcde";
-	cout << a[5] << a[5]<<a[5]<<"f";
+	int a = 2;
+	Dodo d1(4, a);
+	Dodo d2 = d1;
+	Dodo d3{ std::move(d2) };
+	std::cout << d1.getVal() << std::endl;
+	std::cout << d2.getVal() << std::endl;
+	std::cout << d3.getVal() << std::endl;
+	std::cout << "=====" << std::endl;
+	std::cout << *d1.p << std::endl;
+	std::cout << *d2.p << std::endl;
+
+
+
 	return 0;
 }
